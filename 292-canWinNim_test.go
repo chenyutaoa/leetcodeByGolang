fa141855346ceed4ccea3fc9b6c4fbdc24055a44
rudy <40810931+chenyutaoa@users.noreply.github.com>
2021-09-18 11:28:29 +0800

    2021-09-18每日一题

---
+++ 292-canWinNim_test.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import "testing"
+
+func Test_canWinNim(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"",args{n: 4},false},
+		{"",args{n: 1},true},
+		{"",args{n: 2},true},
+		{"",args{n: 8},false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canWinNim(tt.args.n); got != tt.want {
+				t.Errorf("canWinNim() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
