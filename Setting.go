808cc8808747534fe6c74a0231a21ee14077b247
rudy <40810931+chenyutaoa@users.noreply.github.com>
2021-10-19 19:47:48 +0800

    setting

--- Setting.go
+++ Setting.go
@@ -4,3 +4,14 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+type Node struct {
+	Val   int
+	Prev  *Node
+	Next  *Node
+	Child *Node
+}
+type TrieNode struct {
+	isWord       bool
+	nextTrieNode [26]*TrieNode
+}
